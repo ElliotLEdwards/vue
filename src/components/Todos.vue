c02265cf4f1ca9a545f738d309a395d47b7aae9e
Elliot Edwards <elliotedwards@elliots-macbook-pro.home>
2020-05-08 14:58:50 +0200

    Reoared todo delete

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -5,7 +5,7 @@
             <AddTodo class="p-2" v-on:add-todo="addTodo" />
         </div>
         <div v-bind:key="todo.id" v-for="todo in todos">
-            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+            <TodoItem v-bind:todo="todo" v-on:del-todo="deleteTodo(todo.id)" />
         </div>
     </div>    
 </template>
