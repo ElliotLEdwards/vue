7123a6f32f2f64e8f63e7270fecb522ec15f95a6
Elliot Edwards <elliotedwards@elliots-macbook-pro.home>
2020-08-03 20:03:27 +0200

    Added download button for CV

--- src/components/Cv.vue
+++ src/components/Cv.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
+        <form action="http://elliotedwards.com/pdf/CV.pdf">
+            <button type="button submit" target="_blank" class="btn btn-outline-primary download_button">Download</button>
+        </form>
         <pdf
+            class="pdf_area"
             src="http://elliotedwards.com/pdf/CV.pdf"
-            @num-pages="pageCount = $event"
-            @page-loaded="currentPage = $event"
         ></pdf>
     </div>
 </template>
-<style scoped>
 
+<style scoped>
+    .pdf_area {
+        width: 70%;
+        margin: auto;
+    }
+    .download_button {
+        margin: auto;
+    }
 </style>
+
 <script>
 import pdf from 'vue-pdf'
 
